a47d2b77aabe19e1b53972cccc47d13ef26cdf26
witt3rd <witt3rd@witt3rd.com>
2019-10-11 15:37:14 -0700

    code execution

--- src/graphql/Function/index.js
+++ src/graphql/Function/index.js
@@ -1,11 +1,27 @@
+// https://nodejs.org/api/vm.html
+
+const vm = require('vm');
+
+const exec = (input, fn) => {
+  try {
+    const sandbox = { input, output: null };
+    vm.createContext(sandbox);
+    vm.runInContext(fn.code, sandbox);
+    console.log(`sandbox:${fn.name}`, sandbox);
+    return sandbox.output;
+  } catch (ex) {
+    console.log('Ex', ex);
+    throw ex;
+  }
+};
+
 // --- GraphQL resolvers
 
 const resolver = {
   Query: {
-    apply: async (_, { id, params }, { models }) => {
-      const x = await models.Function.findOne({ id });
-      console.log('apply', x);
-      return `apply(id:${id}, params: ${params})`;
+    age: async (_, args, { models }) => {
+      const fn = await models.Function.findOne({ id: '0' });
+      return exec(args, fn);
     },
     list: async (_, args, { models }) => {
       const x = await models.Function.find();
