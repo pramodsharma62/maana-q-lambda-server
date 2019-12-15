51302e5bf1bf7a2f3fe9e4c2689f1bba1c742085
witt3rd <witt3rd@witt3rd.com>
2019-12-15 14:07:29 +0300

    trim results

--- src/runtime/runCode.js
+++ src/runtime/runCode.js
@@ -20,7 +20,7 @@ const runCode = async ({ input, lambda, context }) =>
       const command = ExecutorMap[lambda.runtime.language];
       const cwd = tmpDir;
       const codeFile = path.join(tmpDir, lambda.name);
-      logLambda(lambda, 'runCode', command, codeFile, JSON.stringify(input));
+      // logLambda(lambda, 'runCode', command, codeFile, JSON.stringify(input));
       fs.writeFileSync(codeFile, lambda.code);
 
       const escape = str => str.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
@@ -29,13 +29,13 @@ const runCode = async ({ input, lambda, context }) =>
       const result = [];
       process.stdout.on('data', data => {
         // logLambda(lambda, 'stdout', data.toString());
-        result.push(data.toString());
+        result.push(data.toString().trim());
       });
 
       const log = [];
       process.stderr.on('data', data => {
         // logLambda(lambda, 'stderr', data.toString());
-        log.push(data.toString());
+        log.push(data.toString().trim());
       });
 
       process.on('close', code => {
