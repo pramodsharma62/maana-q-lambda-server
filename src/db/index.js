5f6e158da3e6238cf10ef6066751d5676151bc28
Donald Thompson <witt3rd@witt3rd.com>
2020-02-05 10:51:17 -0800

    better mongo state logging; reconnect on disconnect (#8)

--- src/db/index.js
+++ src/db/index.js
@@ -15,9 +15,40 @@ mongoose.Promise = global.Promise;
 
 const startDB = args => {
   const { user, pwd, url, db } = args;
+
+  const conn = mongoose.connection;
+  conn.on('connecting', function() {
+    console.log('connecting to MongoDB...');
+  });
+
+  conn.on('error', function(error) {
+    console.error('Error in MongoDb connection:', error);
+    mongoose.disconnect();
+  });
+
+  conn.on('connected', function() {
+    console.log('MongoDB connected!');
+  });
+
+  conn.once('open', function() {
+    console.log('MongoDB connection opened!');
+  });
+
+  conn.on('reconnected', function() {
+    console.log('MongoDB reconnected!');
+  });
+
+  conn.on('disconnected', function() {
+    console.log('MongoDB disconnected!');
+    setTimeout(() => startDB(args), 3000);
+  });
+
+  const connStr = `mongodb://${user}:${pwd}@${url}/${db}`;
+  console.log('Connect to', connStr);
   mongoose.connect(
-    `mongodb://${user}:${pwd}@${url}/${db}`,
+    connStr,
     {
+      server: { auto_reconnect: true },
       useNewUrlParser: true
     },
     error => {
