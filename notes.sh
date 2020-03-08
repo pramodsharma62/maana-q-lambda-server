2df46fb6afc1223e54ed498b7ab078947f73c833
Pramod Sharma <pramodsharma62@gmail.com>
2020-03-07 22:13:40 -0800

    updated workflow

--- notes.sh
+++ notes.sh
@@ -9,3 +9,10 @@ services
 kubectl create secret docker-registry services --docker-server=services.azurecr.io --docker-username=services --docker-password=DaBhkCBalRkyvbXC3fg7dMl4BNv+hGSB
 kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "services"}]}'
 kubectl get serviceaccount -o yaml
+
+
+   az ad sp create-for-rbac --name "myApp" --role contributor \
+                            --scopes /subscriptions/c73d0ee9-3ec0-402e-b7eb-2d5a99e9f0f0/resourceGroups/maanaQ \
+                            --sdk-auth
+
+                            
